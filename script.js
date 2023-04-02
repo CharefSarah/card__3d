71e972f098fc5e78c1d3fb40df1df90f667e59a3
CharefSarah <charef.sarah@gmx.fr>
2023-04-02 22:25:24 +0200

    push push

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 import * as THREE from './js/three.module.js';
 import { OrbitControls } from './js/OrbitControls.js';
 
-const canvas = document.getElementById('canvas');
+// Select the container for the scene
+const container = document.getElementById('container');
 
 // Create the scene, camera, and renderer
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, canvas.clientWidth / canvas.clientHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer({ canvas });
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+container.appendChild(renderer.domElement);
 
 // Load the panoramic image and create a texture
 const loader = new THREE.TextureLoader();
-const texture = loader.load('sea.jpg');
+const texture = loader.load('zombie.jpg');
 
 // Create a spherical geometry and map the texture to it
 const geometry = new THREE.SphereGeometry(500, 60, 40);
@@ -31,35 +35,36 @@ camera.position.set(0, 0, 0.1);
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableZoom = false;
 controls.enablePan = false;
+
 controls.rotateSpeed = 0.3;
 
 function onWindowResize() {
-    camera.aspect = canvas.clientWidth / canvas.clientHeight;
+    camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-    renderer.setSize(canvas.clientWidth, canvas.clientHeight);
+    renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 window.addEventListener('resize', onWindowResize, false);
-onWindowResize();
 
 // Animation loop
-function animate() {
+let lastTime = 0;
+const rotationSpeed = 0.00003;
+
+function animate(time) {
+    const delta = time - lastTime;
+    lastTime = time;
     requestAnimationFrame(animate);
+
+    sphere.rotation.y += rotationSpeed * delta;
+
     controls.update();
     renderer.render(scene, camera);
 }
 
-animate();
+animate(0);
 
-function onTiltMouseMove(event) {
-    const tiltRotationX = (event.clientY / window.innerHeight - 0.5) * 50;
-    const tiltRotationY = (event.clientX / window.innerWidth - 0.5) * 50;
 
-    const card = document.querySelector('.card-wrapper');
-    card.style.transform = `rotateX(${tiltRotationX * -1}deg) rotateY(${tiltRotationY}deg)`;
 
-    camera.position.x = tiltRotationY * 0.015; // Divisez par 2 ici
-    camera.position.y = -tiltRotationX * 0.015; // Divisez par 2 ici
-}
 
-document.addEventListener('mousemove', onTiltMouseMove);
+
+
